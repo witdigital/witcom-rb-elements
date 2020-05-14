28b0d36e77369189e6d5226d9521af022b0942e0
Ryan Mitchell <ryanmitchell@gmail.com>
2020-05-14 07:26:05 -0600

    core/list with column attributes for Desktop
    Need to add mobile and update function/variable names.

---
+++ src/assets/scripts/list-columns.js
@@ -0,0 +1,138 @@
+import assign from 'lodash.assign';
+
+const { createHigherOrderComponent } = wp.compose;
+const { Fragment } = wp.element;
+const { InspectorControls } = wp.editor;
+const { PanelBody, SelectControl } = wp.components;
+const { addFilter } = wp.hooks;
+const { __ } = wp.i18n;
+
+// Enable spacing control on the following blocks
+const enableSpacingControlOnBlocks = [
+  "core/list"
+];
+
+// Available spacing control options
+const spacingControlOptions = [
+  {
+    label: __( '1' ),
+    value: '1',
+  },
+  {
+    label: __( '2' ),
+    value: '2',
+  },
+  {
+    label: __( '3' ),
+    value: '3',
+  },
+  {
+    label: __( '4' ),
+    value: '4',
+  },
+];
+
+/**
+ * Add spacing control attribute to block.
+ *
+ * @param {object} settings Current block settings.
+ * @param {string} name Name of block.
+ *
+ * @returns {object} Modified block settings.
+ */
+const addSpacingControlAttribute = ( settings, name ) => {
+  // Do nothing if it's another block than our defined ones.
+  if ( ! enableSpacingControlOnBlocks.includes( name ) ) {
+    return settings;
+  }
+
+  // Use Lodash's assign to gracefully handle if attributes are undefined
+  settings.attributes = assign( settings.attributes, {
+    spacing: {
+      type: 'string',
+      default: spacingControlOptions[ 0 ].value,
+    },
+  } );
+
+  return settings;
+};
+
+addFilter( 'blocks.registerBlockType', 'extend-block-example/attribute/spacing', addSpacingControlAttribute );
+
+/**
+ * Create HOC to add spacing control to inspector controls of block.
+ */
+const withSpacingControl = createHigherOrderComponent( ( BlockEdit ) => {
+  return ( props ) => {
+    // Do nothing if it's another block than our defined ones.
+    if ( ! enableSpacingControlOnBlocks.includes( props.name ) ) {
+      return (
+        <BlockEdit { ...props } />
+      );
+    }
+
+    const { spacing } = props.attributes;
+
+    // add has-spacing-xy class to block
+    if ( spacing ) {
+      props.attributes.className = `has-cols-${ spacing }`;
+    }
+
+    return (
+      <Fragment>
+        <BlockEdit { ...props } />
+        <InspectorControls>
+          <PanelBody
+            title={ __( 'My Spacing Control' ) }
+            initialOpen={ true }
+          >
+            <SelectControl
+              label={ __( 'Spacing' ) }
+              value={ spacing }
+              options={ spacingControlOptions }
+              onChange={ ( selectedSpacingOption ) => {
+                props.setAttributes( {
+                  spacing: selectedSpacingOption,
+                } );
+              } }
+            />
+          </PanelBody>
+        </InspectorControls>
+      </Fragment>
+    );
+  };
+}, 'withSpacingControl' );
+
+addFilter( 'editor.BlockEdit', 'extend-block-example/with-spacing-control', withSpacingControl );
+
+/**
+ * Add margin style attribute to save element of block.
+ *
+ * @param {object} saveElementProps Props of save element.
+ * @param {Object} blockType Block type information.
+ * @param {Object} attributes Attributes of block.
+ *
+ * @returns {object} Modified props of save element.
+ */
+const addSpacingExtraProps = ( saveElementProps, blockType, attributes ) => {
+  // Do nothing if it's another block than our defined ones.
+  if ( ! enableSpacingControlOnBlocks.includes( blockType.name ) ) {
+    return saveElementProps;
+  }
+
+  const margins = {
+    1: '1',
+    2: '2',
+    3: '3',
+    4: '4',
+  };
+
+  if ( attributes.spacing in margins ) {
+    // Use Lodash's assign to gracefully handle if attributes are undefined
+    assign( saveElementProps, { style: { 'column-count': margins[ attributes.spacing ] } } );
+  }
+
+  return saveElementProps;
+};
+
+addFilter( 'blocks.getSaveContent.extraProps', 'extend-block-example/get-save-content/extra-props', addSpacingExtraProps );
