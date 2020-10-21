71e8b224edc07873d77c655df66d668cf3a0e530
Ryan Mitchell <ryanmitchell@gmail.com>
2020-10-21 09:32:37 -0600

    Add SVG support to Layout B

--- app/acf-blocks/witcom-service-boxes/views/layout_style_b.php
+++ app/acf-blocks/witcom-service-boxes/views/layout_style_b.php
@@ -22,21 +22,37 @@
             // vars
             $service_name = get_sub_field('service_name');
             $service_icon = get_sub_field('service_icon');
+            $service_icon_url =  $service_icon['url'];
+            $service_icon_id =  $service_icon['id'];
+            $service_icon_alt =  $service_icon['alt'];
+            $service_icon_path = get_attached_file( $service_icon_id );
             $service_description = get_sub_field('service_description');
             $service_page_link = get_sub_field('service_page_link');
 
+
+            echo file_get_contents($service_icon);
+            include $service_icon_url;
             ?>
 
 
             <a href="<?php echo $service_page_link ?> "class="witcom_services_boxes_services-item service">
 
-
-
                         <?php if($witcom_services_boxes_show_icon = true): ?>
 
                             <?php if( $service_icon ): ?>
                                 <div class="service-image">
-                                    <img src="<?php echo $service_icon['url']; ?>" />
+
+                                    <!--Check if image is an SVG and if so, inline it. -->
+                                    <?php if ( $service_icon['mime_type'] =="image/svg+xml") { ?>
+                                       <div class="svg-wrapper">
+                                            <?php echo file_get_contents( $service_icon_path ) ; ?>
+                                            </div>
+                                        <?php } else { ?>
+                                            <img src="<?php echo $service_icon_url; ?>" alt="<?php echo $service_icon_alt ?>" />
+                                   <?php }; ?>
+
+
+
                                 </div>
 
                             <?php endif; ?>
